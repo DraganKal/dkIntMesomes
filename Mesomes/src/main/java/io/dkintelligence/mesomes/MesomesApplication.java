17414579a42deb749ac6b968494bbf9d77796849
DraganKal <dragan.kalakovic@yahoo.com>
2021-02-04 14:56:04 +0100

    first commit

---
+++ Mesomes/src/main/java/io/dkintelligence/mesomes/MesomesApplication.java
@@ -0,0 +1,13 @@
+package io.dkintelligence.mesomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesomesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesomesApplication.class, args);
+    }
+
+}
---
+++ Mesomes/src/test/java/io/dkintelligence/mesomes/MesomesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dkintelligence.mesomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesomesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
