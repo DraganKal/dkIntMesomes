f4d57493f0fa7d2767008bc9ef4a48ce94afa3b0
DraganKal <dragan.kalakovic@yahoo.com>
2021-02-04 23:57:01 +0100

    Secure our React App(User registration, login, handle routing for expired token, dynamic header based on security state, lock public routes when logged in, SecuredRoutes)

--- mesomes-react-client/src/App.js
+++ mesomes-react-client/src/App.js
@@ -6,6 +6,29 @@ import { Provider } from "react-redux";
 import store from "./store";
 import Header from "./components/Layout/Header";
 import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/SetJWTToken";
+import Landing from "./components/Layout/Landing";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./components/userManagement/Register";
+import Login from "./components/userManagement/Login";
+import { SET_CURRENT_USER } from "./actions/types";
+import { logout } from "./actions/SecurityActions";
+import SecuredRoute from "./securityUtils/SecureRoute";
+
+const jwtToken = localStorage.jwtToken;
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken,
+  });
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    store.dispatch(logout());
+    window.location.href = "/";
+  }
+}
 
 class App extends Component {
   render() {
@@ -14,6 +37,13 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+            {
+              //Public ROutes
+            }
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
             <switch></switch>
           </div>
         </Router>
--- mesomes-react-client/src/components/Layout/Header.js
+++ mesomes-react-client/src/components/Layout/Header.js
@@ -2,15 +2,142 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { logout } from "../../actions/SecurityActions";
 
 class Header extends Component {
+  logout() {
+    this.props.logout();
+    window.location.href = "/";
+  }
   render() {
-    return (
-      <div>
-        <p>header</p>
+    const { validToken, user } = this.props.security;
+
+    const userIsAuthenticated = (
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto topnav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Received Messages
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Sent Messages
+            </a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link btn btn-danger text-white"
+              type="button"
+              to="/logout"
+              data-toggle="modal"
+              data-target="#myModal"
+              onClick={this.logout.bind(this)}
+            >
+              Logout
+            </Link>
+          </li>
+        </ul>
       </div>
+    );
+
+    const userIsNotAuthenticated = (
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto topnav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link btn btn-primary text-white"
+              type="button"
+              to="/login"
+              data-toggle="modal"
+              data-target="#myModal"
+            >
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              className="nav-link btn btn-danger text-white"
+              type="button"
+              to="/register"
+              data-toggle="modal"
+              data-target="#myModal"
+            >
+              Sign up
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+
+    let headerLinks;
+
+    if (validToken && user) {
+      headerLinks = userIsAuthenticated;
+    } else {
+      headerLinks = userIsNotAuthenticated;
+    }
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <Link className="navbar-brand" to="/">
+          MESOMES
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        {headerLinks}
+      </nav>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  security: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  security: state.security,
+});
+
+export default connect(mapStateToProps, { logout })(Header);
